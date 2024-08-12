0763162c5013cf02a7f022db5e94242aa2051459
Spencer <spencerartisan@gmail.com>
2024-08-12 14:33:33 +0100

    Add intelliJ project and deliberate failing test

--- tests.py
+++ tests.py
@@ -7,5 +7,9 @@ class TestHelloWorld(unittest.TestCase):
         import app
         self.assertEqual(app.hello_world(), "Hello, World!")
 
+    def test_hello_world_fail(self):
+        import app
+        self.assertEqual(app.hello_world(), "Goodbye, World!")
+
 if __name__ == '__main__':
     unittest.main()
