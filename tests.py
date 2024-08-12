a7e871b6035b57d1fd235868123b14af85980831
Spencer <spencerartisan@gmail.com>
2024-08-12 15:30:52 +0100

    Remove failing test

--- tests.py
+++ tests.py
@@ -7,9 +7,6 @@ class TestHelloWorld(unittest.TestCase):
         import app
         self.assertEqual(app.hello_world(), "Hello, World!")
 
-    def test_hello_world_fail(self):
-        import app
-        self.assertEqual(app.hello_world(), "Goodbye, World!")
 
 if __name__ == '__main__':
     unittest.main()
--- tests2.py
+++ tests2.py
@@ -4,7 +4,6 @@ import app
 class TestHelloWorld(unittest.TestCase):
 
     def test_hello_world_again(self):
-        import app
         self.assertEqual(app.hello_world(), "Hello, World!")
 
 if __name__ == '__main__':
